41d8891b3a570f958069a0492446063f318ed9a9
takumi333 <135724627+takumi333@users.noreply.github.com>
2024-08-29 20:47:07 +0900

    Revert "Feature/#1 create db"

--- backend/app/models/region.rb
+++
@@ -1,4 +0,0 @@
-class Region < ApplicationRecord
-  has_many :search_historys
-  has_many :weather_data
-end
--- backend/app/models/setting_information.rb
+++
@@ -1,4 +0,0 @@
-class SettingInformation < ApplicationRecord
-  belongs_to :user
-  belongs_to :search_history
-end
